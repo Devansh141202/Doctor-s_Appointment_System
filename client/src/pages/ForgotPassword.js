f6cb0349fa7be829e68ea5f983770930ebbe9267
Ankit-34 <ankitganatra007@gmail.com>
2023-05-27 00:47:25 +0530

    forgot password fixed

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -38,69 +38,76 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          width: "100%",
-          gap: "10px",
-          marginTop: "50px",
-          padding: "10px",
-        }}
-      >
-        <div>
-          <img src={MainLogo} alt="logo" width={"100px"} height={"100px"} />
-        </div>
-        <div
-          style={{
-            fontSize: "40px",
-            fontWeight: "bold",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        >
-          Forgot Password
-        </div>
-        <div
-          style={{
-            marginTop: "20px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-            width: "100%",
-            fontSize: "20px",
-          }}
-        >
-          just type your email, we will send you email to reset your password
-        </div>
-        <div className="forgot-password-card">
-          <Form
-            style={{ display: "flex", flexDirection: "column" }}
-            layout="vertical"
-            onFinish={onFinish}
+      <div className="register-container outer">
+        <div className="register-inside-container inner" style={{
+          height: "80vh"
+        }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              width: "100%",
+              gap: "10px",
+              marginTop: "50px",
+              padding: "10px",
+            }}
           >
-            <Form.Item label="Email" name="email">
-              <Input placeholder="Email" />
-            </Form.Item>
-
-            <button
-              className="register-button"
-              style={{ alignSelf: "center" }}
-              htmlType="submit"
+            <div>
+              <img src={MainLogo} alt="logo" width={"100px"} height={"100px"} />
+            </div>
+            <div
+              style={{
+                fontSize: "40px",
+                fontWeight: "bold",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                width: "100%",
+              }}
             >
-              Send Link to Email
-            </button>
-            <Link to="/login" className="link-for-login">
-              <span> Remember Your Password ?</span>
-              <span> Login here</span>
-            </Link>
-          </Form>
+              Forgot Password
+            </div>
+            <div
+              style={{
+                marginTop: "20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                width: "100%",
+                fontSize: "20px",
+              }}
+            >
+              just type your email, we will send you email to reset your
+              password
+            </div>
+            <div className="forgot-password-card">
+              <Form
+                style={{ display: "flex", flexDirection: "column" }}
+                layout="vertical"
+                onFinish={onFinish}
+              >
+                <Form.Item label="Email : " name="email" style={{display: "flex", flexDirection: "row", alignItems: "center", width:"100%", columnGap:"10px"}}>
+                  <Input placeholder="Email"/>
+                </Form.Item>
+
+                <button
+                  className="register-button"
+                  style={{ alignSelf: "center" }}
+                  htmlType="submit"
+                >
+                  Send Link to Email
+                </button>
+                <Link to="/login" className="link-for-login">
+                  <span> Remember Your Password ?</span>
+                  <span> Login here</span>
+                </Link>
+              </Form>
+            </div>
+          </div>
         </div>
       </div>
     </>
